c3f7ef9827676146ea9120b200011b6b355be46c
Mikolas Janota <mikolas.janota@gmail.com>
2015-10-09 18:41:46 +0100

    Adding exeprimental q2 tactic.

---
+++ src/tactic/smtlogics/q2_tactic.h
@@ -0,0 +1,12 @@
+#ifndef _Q2_TACTIC_H_
+#define _Q2_TACTIC_H_
+#include"tactical.h"
+
+tactic * mk_q2_tactic(ast_manager & m, params_ref const & p);
+
+/*
+ADD_TACTIC("q2", "q2.", "mk_q2_tactic(m, p)")
+*/
+
+#endif
+
