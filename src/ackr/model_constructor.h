85ebeab506178c93351757cc3c27737787434c5f
mikolas <mikolas.janota@gmail.com>
2015-12-07 18:59:49 +0000

    Starting to work on model construction from abstract model.

---
+++ src/ackr/model_constructor.h
@@ -0,0 +1,40 @@
+ /*++
+ Copyright (c) 2015 Microsoft Corporation
+
+ Module Name:
+
+  model_constructor.h
+
+ Abstract:
+   Given a propositional abstraction, attempt to construct a model.
+
+
+ Author:
+
+ Mikolas Janota
+
+ Revision History:
+ --*/
+#ifndef MODEL_CONSTRUCTOR_H_626
+#define MODEL_CONSTRUCTOR_H_626
+#include"ast.h"
+#include"ackr_info.h"
+#include"model.h"
+class model_constructor {
+    private:
+        struct imp;
+        imp *  m_imp;
+    public:
+        model_constructor(ast_manager& m, const ackr_info& info);
+        bool check(model_ref& abstr_model);
+        vector<std::pair<app*, app*>>& get_conflicts() {
+            SASSERT(class_state==CONFLICT);
+            return conflicts;
+        }
+    private:
+        enum {CHECKED, CONFLICT, UNKNOWN} class_state;
+        vector<std::pair<app*,app*>> conflicts;
+        ast_manager& m;
+        const ackr_info& info;
+};
+#endif /* MODEL_CONSTRUCTOR_H_626 */
