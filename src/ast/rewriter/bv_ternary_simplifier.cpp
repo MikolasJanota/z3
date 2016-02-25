65cc858c21bf7e0b38514a67946738c66b85d17c
Mikolas Janota <mikolas.janota@gmail.com>
2016-02-25 15:09:52 +0000

    typo

--- src/ast/rewriter/bv_ternary_simplifier.cpp
+++ src/ast/rewriter/bv_ternary_simplifier.cpp
@@ -461,7 +461,7 @@ unsigned bv_ternary_simplifier::get_num_steps() const {
 }
 
 void bv_ternary_simplifier::collect_statistics(statistics & st) const {
-   st.update("bv ternary sims", m_stats.m_simps);
+   st.update("bv ternary simps", m_stats.m_simps);
 }
 
 void bv_ternary_simplifier::reset_statistics() {
